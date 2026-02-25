de6dea3e002a11b31d966448a176bc167028be3b
Kuldeep <kuldeep.kalita22222@gmail.com>
2026-02-25 07:51:12 +0000

    import { initializeApp, getApps, getApp } from "firebase/app";
    const fi

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-2959754893-c7a47",
-  "appId": "1:156337662657:web:9462a5f220d6d531cf8240",
-  "apiKey": "AIzaSyAMDlgdSf18NDLjMBxlPhZl6PWgvQWYnLQ",
-  "authDomain": "studio-2959754893-c7a47.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "156337662657"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
