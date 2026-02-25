82067d5d6b85240a9427c306b8bcb38c45c11214
Kuldeep <kuldeep.kalita22222@gmail.com>
2026-02-25 08:42:26 +0000

    Missing Suspense boundary with useSearchParams

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import * as React from 'react';
 import { SignupForm } from '@/components/auth/signup-form';
 import { Loader2 } from 'lucide-react';
@@ -15,7 +13,6 @@ function SignupSkeleton() {
     </Card>
   );
 }
-
 
 export default function SignupPage() {
   return (
