a3d4826a91d9afbd5ffa38a3e563ccb4b1d01279
Kuldeep <kuldeep.kalita22222@gmail.com>
2026-02-28 12:08:29 +0000

    const buttonRef = useRef(null);
    ref={composeRefs(forwardedRef, buttonRe

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,19 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import type { Ref } from "react"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function composeRefs<T>(...refs: Ref<T>[]) {
+  return (node: T) => {
+    for (const ref of refs) {
+      if (typeof ref === 'function') {
+        ref(node);
+      } else if (ref !== null) {
+        (ref as React.MutableRefObject<T>).current = node;
+      }
+    }
+  };
 }
